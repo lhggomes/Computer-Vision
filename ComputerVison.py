43e0d37ae96fbc12e0dd475f5393198823bf8087
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-23 08:43:06 -0300

    Creating an Rectangle
    Creating an Rectangle behind the face of the dected face

--- ComputerVison.py
+++ ComputerVison.py
@@ -1,3 +1,4 @@
+#Project on Computer Vision
 import numpy as np 
 import cv2 as cv 
 
@@ -6,3 +7,9 @@ face_classifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 #Using the photo on a Gray Scale 
 imageValid = cv.imread('tony.jpg')
 grayImage = cv.cvtColor(imageValid, cv.COLOR_BGR2GRAY)
+
+#Creating an Rectangle on the Face of the person
+faces = face_classifier.detectMultiScale(grayImage, 1.3, 5)
+
+for (x,y,w,h) in faces: 
+    cv.rectangle(grayImage,(x,y), (x+w, y+h), (255,0,0), 2)
