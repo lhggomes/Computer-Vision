47f1de2a680e5582e70b7d2aff9266ace8b3412d
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-23 08:37:30 -0300

    Implemation of Gray Scale

--- ComputerVison.py
+++ ComputerVison.py
@@ -3,4 +3,6 @@ import cv2 as cv
 
 face_classifier = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+#Using the photo on a Gray Scale 
 imageValid = cv.imread('tony.jpg')
+grayImage = cv.cvtColor(imageValid, cv.COLOR_BGR2GRAY)
