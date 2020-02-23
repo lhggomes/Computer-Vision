c05c4e6862ccebc9bb867df3a0a07ee31a5a6dd5
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-23 08:46:20 -0300

    Commeting code

--- ComputerVison.py
+++ ComputerVison.py
@@ -14,6 +14,10 @@ faces = face_classifier.detectMultiScale(grayImage, 1.3, 5)
 for (x,y,w,h) in faces: 
     cv.rectangle(imageValid,(x,y), (x+w, y+h), (255,0,0), 2)
 
+#Show the photo on the screen for the user
+
 cv.imshow('imagem', imageValid)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+
