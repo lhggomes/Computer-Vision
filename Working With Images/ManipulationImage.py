22a6fe3940893cab95755edb2249f12d74325a1b
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-25 06:37:45 -0300

    Function ReadPixels
    Function to show the information who's loaded on the pixels.
    
    (Y,X), and Colors Channels.

--- Working With Images/ManipulationImage.py
+++ Working With Images/ManipulationImage.py
@@ -7,3 +7,5 @@ soccerPicture = cv2.imread('soccer.jpg')
 #Printing the data of the picture
 print (soccerPicture.shape)
 
+#Show the information about the Pixels 
+print (soccerPicture.item(0,0,1)) # 0 = Blue Channel / 1 = Green Channel / 2 = Red Channel 
