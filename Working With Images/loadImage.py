e296ce58994d1589dc7f0fba6f26544dad24b033
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-24 21:15:45 -0300

    Coding the Function
    Coding the function to read some image from the disk and show it on the screen.

--- Working With Images/loadImage.py
+++ Working With Images/loadImage.py
@@ -1,3 +1,12 @@
-import sys
-from PIL import Image 
+import cv2
 
+#Variable to storage the Image from the disk 
+imageLoad = cv2.imread("test.jpg", 0)
+
+#Show the image on Screen
+cv2.imshow("ImagemCarregada", imageLoad)
+
+#Next Function it's to prevent the window to close before the user do it
+cv2.waitKey(0)
+#Function to Clear all the windows after the end of the Program
+cv2.destroyAllWindows()
