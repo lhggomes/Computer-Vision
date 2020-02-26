f7cf1bd8c325544d96982419ceac65a06f6f29aa
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-02-26 18:40:23 -0300

    Img Show (Histogram)

--- Working With Images/imageHistrogram.py
+++ Working With Images/imageHistrogram.py
@@ -2,10 +2,13 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt 
 
-image = cv2.imread("soccer.jpg", 0)
+#Reading the Image from the disk 
+image = cv2.imread("soccer.jpg", 0) 
+#Crating the histogram
+#In this function, We needed to use another funciont (ravel) to transform the data from a matrice 2x2 to a Vector
 plt.hist(image.ravel(), 256, [0,256])
 cv2.imshow("Imagem Original", image)
+#Show the data in the screen 
 plt.show()
-
 cv2.waitKey()
 cv2.destroyAllWindows()
