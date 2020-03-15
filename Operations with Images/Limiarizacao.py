723943da231010ac51582a44e20572d90ce3e68a
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-03-15 20:54:58 -0300

    Tho ways to control de r

--- Operations with Images/Limiarizacao.py
+++ Operations with Images/Limiarizacao.py
@@ -6,8 +6,12 @@ img = cv2.imread("Photos/moedas.jpg",0)
 adp1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
                              cv2.THRESH_BINARY, 11, 2)
 
+adp2 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                             cv2.THRESH_BINARY, 11, 2)
+
 cv2.imshow("original", img)
-cv2.imshow("limiarização", adp1)
+cv2.imshow("limiMedia", adp1)
+cv2.imshow("LimGaussian", adp2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
