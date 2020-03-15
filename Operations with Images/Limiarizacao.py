280c8b14486c8742a549b7449b77ba48781bb235
Lucas Gomes <48351001+lhggomes@users.noreply.github.com>
2020-03-15 20:49:58 -0300

    Creatign function

--- Operations with Images/Limiarizacao.py
+++ Operations with Images/Limiarizacao.py
@@ -1,2 +1,13 @@
 import cv2
-import numpy as np
+
+img = cv2.imread("Photos/moedas.jpg",0)
+
+
+adp1 = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                             cv2.THRESH_BINARY, 11, 2)
+
+cv2.imshow("original", img)
+cv2.imshow("limiarização", adp1)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
