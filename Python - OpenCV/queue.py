cac019dee9e59091ed38a51d2a729b0de5514c8e
Lucas Gomes <lucas.henrique.s.go@gmail.com>
2020-03-17 23:16:26 -0300

    Realizando as operações na fila. Primeiro a entrar será o primeiro a sair.

--- Python - OpenCV/queue.py
+++ Python - OpenCV/queue.py
@@ -1,11 +1,32 @@
-class fila:
+class Fila:
     def __init__(self):
         self.fila =  []
 
     def inserir(self,n):
         self.fila.append(n)
+    #A Funcção de inserçao utlizrá o append
 
     def excluir(self):
-        del self.fila[0]
+        if not self.vazia():
+          return self.fila.pop(0)
 
+
+    def tamanho(self):
+        return len(self.fila)
+
+    def vazia(self):
+        return self.tamanho()== 0
+
+
+
+fila = Fila()
+fila.vazia()
+
+fila.inserir(1)
+fila.inserir(2)
+fila.inserir(3)
+fila.excluir()
+
+print(fila.tamanho())
+print(fila.vazia())
 
