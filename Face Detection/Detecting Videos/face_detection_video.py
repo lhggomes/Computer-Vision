eb53958fac15a7c500eba4c3c58f85289c63e34d
Lucas Henrique Silva Gomes <lucas.gomes-partner@aperam.com>
2020-07-27 08:05:38 -0300

    Directory working with circle detect

--- Face Detection/Detecting Videos/face_detection_video.py
+++ Face Detection/Detecting Videos/face_detection_video.py
@@ -2,24 +2,23 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-video_file = 'vasilha.mp4'
-cap = cv2.VideoCapture('vasilha.mp4')
+cap = cv2.VideoCapture(0)
 
 while True:
-    #Reading the Frame
+    # Reading the Frame
     _, img = cap.read()
-    #Gray Scale
+    # Gray Scale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #Detect Faces
+    # Detect Faces
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    #Draw for each face
-    for (x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+    # Draw for each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 128, 0), 2)
 
     cv2.imshow('img', img)
 
     k = cv2.waitKey(30) & 0xff
-    if k==27:
+    if k == 27:
         break
 
 cap.release()
